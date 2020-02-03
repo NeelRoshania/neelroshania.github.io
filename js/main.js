83e35827785691cb8d16431c33019b1ae0088e4a
Neel <nroshania@gmail.com>
2020-02-02 22:31:31 -0800

    Published calibration feature

--- js/main.js
+++ js/main.js
@@ -9,6 +9,19 @@
 //     - Convert json to csv 
 //     - Export to csv for R
 
+function getMean(data_array){
+    return data_array.reduce(function (a, b) {
+        return Number(a) + Number(b);
+    }) / data_array.length;
+}
+
+
+function getStd(data_array){
+    return Math.sqrt(data_array.reduce(function (sq, n) {
+        return sq + Math.pow(n - getMean(data_array), 2);
+    }, 0) / (data_array.length - 1));
+}
+
 function convertToCSV(objArray) {
     var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
     var str = '';
@@ -17,13 +30,10 @@ function convertToCSV(objArray) {
         var line = '';
         for (var index in array[i]) {
             if (line != '') line += ','
-
             line += array[i][index];
         }
-
         str += line + '\r\n';
     }
-
     return str;
 }
 
@@ -63,6 +73,69 @@ var headers = {
     responses: "Participant Response"
 };
 
+// Draw calibration bars
+var draw = function(analyser, canvasCtx) {
+
+    analyser.fftSize = 256;
+    var bufferLengthAlt = analyser.frequencyBinCount;
+    console.log(bufferLengthAlt);
+    var dataArrayAlt = new Uint8Array(bufferLengthAlt);
+
+    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
+    // Draw bars
+    var drawAlt = function() {
+        drawVisual = requestAnimationFrame(drawAlt);
+        analyser.getByteFrequencyData(dataArrayAlt);
+        canvasCtx.fillStyle = 'rgb(255, 255, 255)';
+        canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+        var barWidth = (WIDTH / bufferLengthAlt) * 2.5;
+        var barHeight;
+        var x = 0;
+
+        //   Use average and std of frequency magnitudes to check microphone, calibrate user voice and detect a noisy environment
+        // decibel_approx.textContent = getMean(dataArrayAlt)
+        // decibel_variation.textContent = getStd(dataArrayAlt)
+
+        // Manage calibration output
+        frequency_mean = getMean(dataArrayAlt);
+        frequency_std = getStd(dataArrayAlt);
+
+        // Check microphone output, if ok then make recording unit visible
+        if (frequency_mean > 0) {
+            decibel_approx.style.color = "#61d800";
+            decibel_approx.textContent = "Microphone detected";
+            recording_unit.style.visibility = 'visible'
+
+            // Calibrate voice
+            if (frequency_mean > 25) {
+                decibel_approx.textContent = "Voice amplitude OK";
+            } 
+            
+            // Detect environment
+            if (frequency_mean > 50 && (frequency_std > 65)) {
+                decibel_approx.style.color = "#ee6002"
+                decibel_approx.textContent = "Too noisy to talk";
+            } 
+        } else {
+            decibel_approx.style.color = "#ee6002"
+            decibel_approx.textContent = "No sound detected"
+        };
+
+        // Manage bar chart visualization
+        for(var i = 0; i < bufferLengthAlt; i++) {
+            barHeight = dataArrayAlt[i];
+
+            canvasCtx.fillStyle = 'rgb(' + (barHeight+100) + ',50,50)';
+            canvasCtx.fillRect(x,HEIGHT-barHeight/2.5,barWidth,barHeight/2.5);
+
+            x += barWidth + 1;
+            };
+        };
+        drawAlt();
+    };
+
 // Start main
 
 // Initialize object DOMS
@@ -73,10 +146,14 @@ const session_status = document.getElementById('session-status');
 const fetch_results = document.getElementById("complete");
 const r_code = document.getElementById('r_code');
 const export_csv = document.getElementById("export-csv");
+const calibration = document.getElementById("calibration");
+const recording_unit = document.getElementById("recording-unit");
 var question = document.getElementById('question');
 var results_card = document.getElementById("results");
 var table = document.getElementById("data-table");
-var output = document.getElementById('output');
+var output = document.getElementById('output'); 
+var decibel_approx = document.getElementById('decibel_approx'); 
+var decibel_variation = document.getElementById('decibel_variation'); 
 
 // Initialize recorder and DOM elements
 var _speech_transcript, text, space
@@ -88,7 +165,46 @@ var json_to_table = [];
 results_card.style.visibility = 'hidden'
 fetch_results.style.visibility = 'hidden'
 r_code.style.visibility = 'hidden'
+recording_unit.style.visibility = 'hidden'
 
+// Start calibration
+//     - To extract data from your audio source, you need an AnalyserNode
+//     - This node is then connected to your audio source at some point between your source and your destination
+//     - The analyser node will then capture audio data using a Fast Fourier Transform (fft) in a certain frequency domain, depending on what you specify as the AnalyserNode.fftSize property value 
+//     - To capture data, you need to use the methods AnalyserNode.getFloatFrequencyData() and AnalyserNode.getByteFrequencyData() to capture frequency data, and AnalyserNode.getByteTimeDomainData() and AnalyserNode.getFloatTimeDomainData() to capture waveform data.
+//     - These methods copy data into a specified array, so you need to create a new array to receive the data before invoking one.
+//     - Source: https://developer.mozilla.org/en-US/docs/Web/API/AnalyserNode/getByteFrequencyData
+    
+calibration.onclick = function(stream) {
+    if (navigator.mediaDevices.getUserMedia) {
+
+        // setup canvas object properties
+        console.log('getUserMedia supported.');
+        var canvas = document.querySelector('.visualizer');
+        var canvasCtx = canvas.getContext("2d");
+        WIDTH = canvas.width;
+        HEIGHT = canvas.height;
+
+        // Detect streaming content using WebAudio API
+        navigator.mediaDevices.getUserMedia ({ audio: true, video: false })
+           .then(
+             function(stream) {
+                var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+                var analyser = audioCtx.createAnalyser();
+                source = audioCtx.createMediaStreamSource(stream);
+                source.connect(analyser);
+                analyser.fftSize = 2048;
+
+                // Start drawing calibration rectangles
+                canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+                draw(analyser, canvasCtx);
+           })
+           .catch( function(err) { console.log('The following gUM error occured: ' + err);})
+     } else {
+        alert("Hmm...are you a MAC user? I think you're using a browser that doesn't support our tech. Try using Chrome V79.0.")
+        console.log('getUserMedia not supported on this browser.');
+     }
+}
 
 // Start recording, only if a question number has been submitted
 recorder.onclick = function() {
